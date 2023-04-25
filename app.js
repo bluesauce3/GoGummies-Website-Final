4a223dfef62060d3685dec6f29d338a8ce7f3247
Theo <106856604+bluesauce3@users.noreply.github.com>
2023-04-26 06:54:12 +1200

    added all home sections and some about sections

---
+++ app.js
@@ -0,0 +1,8 @@
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+
+function smoothScroll() {
+    document.documentElement.style.scrollBehavior = "smooth";
+}
