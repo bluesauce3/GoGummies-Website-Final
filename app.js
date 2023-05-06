9db87edc5235c62a3befcef7edfae1d5a1412812
Theo <106856604+bluesauce3@users.noreply.github.com>
2023-05-06 18:30:34 +1200

    finished contact form and added new colours

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
 function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
-  }
+}
 
 function smoothScroll() {
     document.documentElement.style.scrollBehavior = "smooth";
+}
+
+function saveFormData(event) {
+  event.preventDefault();
+  let formData = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value
+  };
+  let data = JSON.stringify(formData);
+  localStorage.setItem("formData", data);
+  alert("Form data saved successfully!");
+  document.getElementById("name").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("message").value = "";
+}
+
+function getFormData() {
+  let savedData = localStorage.getItem("formData");
+    if (savedData) {
+	    let formData = JSON.parse(savedData);
+	    return(formData);
+}
 }
